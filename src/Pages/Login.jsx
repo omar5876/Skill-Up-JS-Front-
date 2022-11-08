3885bbd15e05f8151497524b7eea09558f80f561
martinrotela <martinrotela47@gmail.com>
2022-11-08 03:12:29 -0300

    fix: login page import

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,52 +1,46 @@
-import React from "react";
-import Formulario from "../Components/Formularios/Formulario";
-import { Container, Box, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Typography, Button, CardMedia } from '@mui/material';
 
-import loginWallet from "../assets/login-wallet.jpeg";
-
-import LoginForm from "../Components/Formularios/LoginForm/LoginForm";
-import { Box, Typography, Button, CardMedia } from "@mui/material";
-import { Link } from "react-router-dom";
-
-import loginWallet from "../assets/login-wallet.jpeg";
+import LoginForm from '../Components/Formularios/LoginForm/LoginForm';
+import loginWallet from '../assets/login-wallet.jpeg';
 
 function Login() {
-  return (
-    <Box
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr",
-        width: "100%",
-      }}
-    >
-      <CardMedia
-        component="img"
-        alt="phone wallet app"
-        sx={{ height: "100vh" }}
-        image={loginWallet}
-      />
-      <Box
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h5" style={{ marginBottom: "1em" }}>
-          Ingrese sus datos para iniciar sesion
-        </Typography>
-        <LoginForm />
-        <Typography variant="subtitle1">
-          ¿No tienes cuenta?
-          <Button component={Link} to="/register" underline="none">
-            Registrarse
-          </Button>
-        </Typography>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box
+            style={{
+                display: 'grid',
+                gridTemplateColumns: '1fr 1fr',
+                width: '100%',
+            }}
+        >
+            <CardMedia
+                component="img"
+                alt="phone wallet app"
+                sx={{ height: '100vh' }}
+                image={loginWallet}
+            />
+            <Box
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-evenly',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="h5" style={{ marginBottom: '1em' }}>
+                    Ingrese sus datos para iniciar sesion
+                </Typography>
+                <LoginForm />
+                <Typography variant="subtitle1">
+                    ¿No tienes cuenta?
+                    <Button component={Link} to="/register" underline="none">
+                        Registrarse
+                    </Button>
+                </Typography>
+            </Box>
+        </Box>
+    );
 }
 
 export default Login;
