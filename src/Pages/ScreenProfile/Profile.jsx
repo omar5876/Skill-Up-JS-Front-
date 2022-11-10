912d9401670d9dec321cd1d825c35ca2d0084d93
Maximiliano Ferrioli <maxiferrioli.97@gmail.com>
2022-11-10 14:41:09 -0300

    add components MUI

--- src/Pages/ScreenProfile/Profile.jsx
+++ src/Pages/ScreenProfile/Profile.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions } from "@mui/material";
+import Button from "../../Components/Button/Button";
 
-export const Profile = () => {
-  return <>Profile</>;
-};
+function Profile() {
+  return (
+    <Card>
+      {/* Avatar */}
+      <CardActionArea>
+        <CardContent>
+          <Typography sx={{ m: 6 }} variant="h6">
+            CAMPOS
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button text="Editar" />
+        <Button text="Eliminar" />
+      </CardActions>
+    </Card>
+  );
+}
+
+export default Profile;
