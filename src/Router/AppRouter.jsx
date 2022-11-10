f2c2fbcf7ec83389a1b9e43b24a0727d3ecb26f4
martinrotela <martinrotela47@gmail.com>
2022-11-10 13:35:26 -0300

    feat: login persistence

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from '../Components/Global/Footer/Footer';
 import Header from '../Components/Global/Header/Header';
@@ -6,12 +6,16 @@ import WalletRouter from './WalletRouter';
 import PrivateRoute from './PrivateRoute';
 import AuthRouter from './AuthRouter';
 import Home from '../Pages/Home';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { renewUser } from '../features/auth/thunks';
 
 function AppRouter() {
+    const dispatch = useDispatch();
     const { id } = useSelector((state) => state.auth.user);
 
-    // Le agrego exact path auth/* para poder bloquear todo lo que vaya adelante de eso.
+    useEffect(() => {
+        dispatch(renewUser());
+    }, []);
 
     return (
         <Router>
