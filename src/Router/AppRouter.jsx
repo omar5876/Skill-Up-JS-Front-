e0b22f6ab27b4c6c7ebd6fd579121877421d3191
martinrotela <martinrotela47@gmail.com>
2022-11-10 13:19:57 -0300

    feat: Protect routes using redux

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,33 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "../Components/Global/Footer/Footer";
-import Header from "../Components/Global/Header/Header";
-import WalletRouter from "./WalletRouter";
-import PrivateRoute from "./PrivateRoute";
-import AuthRouter from "./AuthRouter";
-import Home from "../Pages/Home";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from '../Components/Global/Footer/Footer';
+import Header from '../Components/Global/Header/Header';
+import WalletRouter from './WalletRouter';
+import PrivateRoute from './PrivateRoute';
+import AuthRouter from './AuthRouter';
+import Home from '../Pages/Home';
+import { useSelector } from 'react-redux';
 
 function AppRouter() {
-  // Le agrego exact path auth/* para poder bloquear todo lo que vaya adelante de eso.
+    const { id } = useSelector((state) => state.auth.user);
 
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path="auth/*" element={<AuthRouter />} />
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/*"
-          element={
-            <PrivateRoute>
-              <WalletRouter />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    // Le agrego exact path auth/* para poder bloquear todo lo que vaya adelante de eso.
+
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route exact path="auth/*" element={<AuthRouter />} />
+                <Route path="/" element={<Home />} />
+                <Route
+                    path="/*"
+                    element={
+                        <PrivateRoute isAuth={!!id}>
+                            <WalletRouter />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default AppRouter;
--- src/Router/PrivateRoute.jsx
+++ src/Router/PrivateRoute.jsx
@@ -1,16 +1,8 @@
-import React, { useState, useEffect } from "react";
-import Login from "../Pages/Login";
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = (props) => {
-  // Aca debemos recibir si el user esta autenticado y luego lo guardamos en LocalStorage. Si el user no esta autenticado, te redirige a Login. Si esta autenticado, va a poder acceder a las children.
-
-  const { isAuth, setIsAuth } = useState(null);
-
-  useEffect(() => {
-    localStorage.getItem("loginState", isAuth);
-  }, []);
-
-  return isAuth ? props.children : <Login />;
+const PrivateRoute = ({ isAuth }) => {
+    return isAuth ? props.children : <Navigate to="auth/login" />;
 };
 
 export default PrivateRoute;
