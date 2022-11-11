a05bfbe97667dbdf5366031b5b8c9e7b5990a4d3
martinrotela <martinrotela47@gmail.com>
2022-11-11 16:00:35 -0300

    fix: admin protected route

--- src/Router/AdminRoute.jsx
+++ src/Router/AdminRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const AdminRoute = ({ isAdmin, children }) => {
-    return isAdmin ? children : <Navigate to="/balance" />;
+    return isAdmin === 'ADMIN' ? children : <Navigate to="/balance" />;
 };
 
 export default AdminRoute;
