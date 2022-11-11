c916859dd6a8a56863e4e6f148b74e0a3adf5c1d
martinrotela <martinrotela47@gmail.com>
2022-11-11 19:00:32 -0300

    feat: logout button

--- src/Components/Global/Header/Header.jsx
+++ src/Components/Global/Header/Header.jsx
@@ -1,44 +1,55 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  Box,
-  Toolbar,
-  Container,
-  Button,
-  MenuItem,
-  AppBar,
-} from "@mui/material";
+    Box,
+    Toolbar,
+    Container,
+    Button,
+    MenuItem,
+    AppBar,
+} from '@mui/material';
 
-import items from "./Items";
-import { Link } from "react-router-dom";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import items from './Items';
+import { Link } from 'react-router-dom';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import { useDispatch } from 'react-redux';
+import { startLogout } from '../../../features/auth/thunks';
 
 console.log(items);
 
 function Header() {
-  return (
-    <AppBar position="static" color="default" style={{ padding: "10px" }}>
-      <Container maxWidth="xl">
-        <Toolbar
-          disableGutters
-          sx={{ display: "flex", justifyContent: "space-between" }}
-        >
-          <Box>
-            <Link to="/wallet">
-              <AccountBalanceIcon fontSize="large" />
-            </Link>
-          </Box>
-          <Box>
-            {items.map((item) => {
-              return (
-                <Button>
-                  <Link to={item.path}>{item.name}</Link>
-                </Button>
-              );
-            })}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(startLogout());
+    };
+
+    return (
+        <AppBar position="static" color="default" style={{ padding: '10px' }}>
+            <Container maxWidth="xl">
+                <Toolbar
+                    disableGutters
+                    sx={{ display: 'flex', justifyContent: 'space-between' }}
+                >
+                    <Box>
+                        <Link to="/wallet">
+                            <AccountBalanceIcon fontSize="large" />
+                        </Link>
+                    </Box>
+                    <Box>
+                        {items.map((item) => {
+                            return (
+                                <Button>
+                                    <Link to={item.path}>{item.name}</Link>
+                                </Button>
+                            );
+                        })}
+                        <Button>
+                            <Link onClick={handleLogout}>Salir</Link>
+                        </Button>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
 export default Header;
--- src/Components/Global/Header/Items.jsx
+++ src/Components/Global/Header/Items.jsx
@@ -1,28 +1,24 @@
 const routes = [
-  {
-    name: "home",
-    path: "/wallet",
-  },
-  {
-    name: "Cargar Saldo",
-    path: "wallet/cargar-saldo",
-  },
-  {
-    name: "Balance",
-    path: "wallet/balance",
-  },
-  {
-    name: "Enviar Dinero",
-    path: "wallet/enviar-dinero",
-  },
-  {
-    name: "Mi Perfil",
-    path: "wallet/mi-perfil",
-  },
-  {
-    name: "Salir",
-    path: "/logout",
-  },
+    {
+        name: 'home',
+        path: '/wallet',
+    },
+    {
+        name: 'Cargar Saldo',
+        path: 'wallet/cargar-saldo',
+    },
+    {
+        name: 'Balance',
+        path: 'wallet/balance',
+    },
+    {
+        name: 'Enviar Dinero',
+        path: 'wallet/enviar-dinero',
+    },
+    {
+        name: 'Mi Perfil',
+        path: 'wallet/mi-perfil',
+    },
 ];
 
 export default routes;
