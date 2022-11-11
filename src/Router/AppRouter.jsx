1fc2caae75f21f9efe7933b79f0a9d689984be4a
martinrotela <martinrotela47@gmail.com>
2022-11-11 16:21:41 -0300

    fix: redirect bugs

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from 'react-router-dom';
 import WalletRouter from './WalletRouter';
 import PublicRoutes from './PublicRoutes';
 import PrivateRoute from './PrivateRoute';
@@ -43,6 +48,7 @@ function AppRouter() {
                     </PrivateRoute>
                 }
             />
+            <Route path="/*" element={<Navigate to="/auth/login" />} />
         </Routes>
     );
 }
--- src/Router/PublicRoutes.jsx
+++ src/Router/PublicRoutes.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PublicRoutes = ({ isAuth, children }) => {
-    return !isAuth ? children : <Navigate to="/" />;
+    return !isAuth ? children : <Navigate to="/wallet/" />;
 };
 
 export default PublicRoutes;
