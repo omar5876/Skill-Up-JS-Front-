c11e6142afc9ecabe04f5acc9b93d3c72b53cc06
martinrotela <martinrotela47@gmail.com>
2022-11-12 09:17:38 -0300

    fix: navbar buttons

--- src/Components/Global/Header/Header.jsx
+++ src/Components/Global/Header/Header.jsx
@@ -1,44 +1,46 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  Box,
-  Toolbar,
-  Container,
-  Button,
-  MenuItem,
-  AppBar,
-} from "@mui/material";
+    Box,
+    Toolbar,
+    Container,
+    Button,
+    MenuItem,
+    AppBar,
+} from '@mui/material';
 
-import items from "./Items";
-import { Link } from "react-router-dom";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-
-console.log(items);
+import items from './Items';
+import { Link } from 'react-router-dom';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import { useSelector } from 'react-redux';
 
 function Header() {
-  return (
-    <AppBar position="static" color="default" style={{ padding: "10px" }}>
-      <Container maxWidth="xl">
-        <Toolbar
-          disableGutters
-          sx={{ display: "flex", justifyContent: "space-between" }}
-        >
-          <Box>
-            <Link to="/wallet">
-              <AccountBalanceIcon fontSize="large" />
-            </Link>
-          </Box>
-          <Box>
-            {items.map((item) => {
-              return (
-                <Button>
-                  <Link to={item.path}>{item.name}</Link>
-                </Button>
-              );
-            })}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+    const { uid } = useSelector((state) => state.auth);
+    return (
+        <AppBar position="static" color="default" style={{ padding: '10px' }}>
+            <Container maxWidth="xl">
+                <Toolbar
+                    disableGutters
+                    sx={{ display: 'flex', justifyContent: 'space-between' }}
+                >
+                    <Box>
+                        <Link to="/wallet">
+                            <AccountBalanceIcon fontSize="large" />
+                        </Link>
+                    </Box>
+                    {uid && (
+                        <Box>
+                            {items.map((item) => {
+                                return (
+                                    <Button>
+                                        <Link to={item.path}>{item.name}</Link>
+                                    </Button>
+                                );
+                            })}
+                        </Box>
+                    )}
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
 export default Header;
