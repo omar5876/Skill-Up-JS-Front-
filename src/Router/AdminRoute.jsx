67fec861b68984cd0d86a6621d5bf904ce624417
martinrotela <martinrotela47@gmail.com>
2022-11-12 13:27:39 -0300

    fix: routers

--- src/Router/AdminRoute.jsx
+++ src/Router/AdminRoute.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const AdminRoute = ({ isAdmin, children }) => {
-    return isAdmin === 'ADMIN' ? children : <Navigate to="/balance" />;
+const AdminRoute = ({ role, children }) => {
+    return role === 'ADMIN' ? children : <Navigate to="/wallet/" />;
 };
 
 export default AdminRoute;
