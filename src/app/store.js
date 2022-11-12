201b2bf95693fafd3698b596320aaef7d3216355
martinrotela <martinrotela47@gmail.com>
2022-11-12 16:12:09 -0300

    connect users dashboard

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authSlice } from '../features/auth/authSlice';
 import { transactionsSlice } from '../features/transactions/transactionsSlice';
+import { usersSlice } from '../features/users/usersSlice';
 
 export default configureStore({
     reducer: {
         auth: authSlice.reducer,
         transactions: transactionsSlice.reducer,
+        users: usersSlice.reducer,
     },
 });
