d4bd7ae91a581abe135e48ecc668ef05f288835e
martinrotela <martinrotela47@gmail.com>
2022-11-13 09:54:18 -0300

    connect transactions delete button

--- src/Components/Dashboard/TransactionsDashboard/TableRow.jsx
+++ src/Components/Dashboard/TransactionsDashboard/TableRow.jsx
@@ -1,49 +1,63 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import { Button, TableCell, TableRow } from "@mui/material";
+import { Button, TableCell, TableRow } from '@mui/material';
 
-import { Link } from "react-router-dom";
+import { deleteTransaction } from '../../../features/transactions/thunks.js';
+import { alertWarning } from '../../Alert/Alert';
 
 function RowTable({ item }) {
-  const [editarTransaccion, setEditarTransaccion] = useState();
+    const dispatch = useDispatch();
+    const [editarTransaccion, setEditarTransaccion] = useState();
 
-  const onPressEdit = () => {
-    setEditarTransaccion(item);
-  };
+    const onPressEdit = () => {
+        setEditarTransaccion(item);
+    };
 
-  return (
-    <>
-      <TableRow
-        key={item.id}
-        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-        hover
-      >
-        <TableCell component="th" scope="row">
-          {item.description}
-        </TableCell>
-        <TableCell align="right">{item.amount}</TableCell>
-        <TableCell align="right">{item.createdAt}</TableCell>
-        <TableCell align="right">{item.userId}</TableCell>
-        <TableCell align="right">{item.categoryId}</TableCell>
-        <TableCell align="right">
-          <Button>
-            <Link to={`/editar/${item.id}`} onClick={onPressEdit}>
-              Editar
-            </Link>
-          </Button>
-        </TableCell>
+    const handleDelete = (id) => {
+        dispatch(deleteTransaction(id));
+    };
 
-        <TableCell align="right">
-          {" "}
-          <Button
-          //   onClick={() => cancelarTurno(item.id)}
-          >
-            Borrar
-          </Button>
-        </TableCell>
-      </TableRow>
-    </>
-  );
+    return (
+        <>
+            <TableRow
+                key={item.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                hover
+            >
+                <TableCell component="th" scope="row">
+                    {item.description}
+                </TableCell>
+                <TableCell align="right">{item.amount}</TableCell>
+                <TableCell align="right">{item.createdAt}</TableCell>
+                <TableCell align="right">{item.userId}</TableCell>
+                <TableCell align="right">{item.categoryId}</TableCell>
+                <TableCell align="right">
+                    <Button>
+                        <Link to={`/editar/${item.id}`} onClick={onPressEdit}>
+                            Editar
+                        </Link>
+                    </Button>
+                </TableCell>
+
+                <TableCell align="right">
+                    {' '}
+                    <Button
+                        onClick={() =>
+                            alertWarning(
+                                item.description,
+                                item.id,
+                                handleDelete
+                            )
+                        }
+                    >
+                        Borrar
+                    </Button>
+                </TableCell>
+            </TableRow>
+        </>
+    );
 }
 
 export default RowTable;
